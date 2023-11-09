9f81fabbbb229ecea58ba13bd4056e4c2a0e8677
chenying09 <chenying09@kuaishou.com>
2023-11-09 15:14:27 +0800

    ts

---
+++ package/src/android/RNCSliderNativeComponent.ts
@@ -0,0 +1,44 @@
+import type {ColorValue, HostComponent, ViewProps} from 'react-native';
+//@ts-ignore
+import type {ImageSource} from 'react-native/Libraries/Image/ImageSource';
+import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
+import type {
+  Float,
+  BubblingEventHandler,
+  WithDefault,
+} from 'react-native/Libraries/Types/CodegenTypes';
+
+type Event = Readonly<{
+  value: Float;
+  fromUser?: boolean;
+}>;
+
+interface NativeProps extends ViewProps {
+  accessibilityUnits?: string;
+  accessibilityIncrements?: ReadonlyArray<string>;
+  disabled?: WithDefault<boolean, false>;
+  enabled?: WithDefault<boolean, true>;
+  inverted?: WithDefault<boolean, false>;
+  vertical?: boolean;
+  tapToSeek?: WithDefault<boolean, false>;
+  maximumTrackImage?: ImageSource;
+  maximumTrackTintColor?: ColorValue;
+  maximumValue?: Float;
+  minimumTrackImage?: ImageSource;
+  minimumTrackTintColor?: ColorValue;
+  minimumValue?: Float;
+  onChange?: BubblingEventHandler<Event>;
+  onRNCSliderSlidingStart?: BubblingEventHandler<Event>;
+  onRNCSliderSlidingComplete?: BubblingEventHandler<Event>;
+  onRNCSliderValueChange?: BubblingEventHandler<Event>;
+  step?: Float;
+  testID?: string;
+  thumbImage?: ImageSource;
+  thumbTintColor?: ColorValue;
+  trackImage?: ImageSource;
+  value?: Float;
+}
+
+export default codegenNativeComponent<NativeProps>(
+  'RNCSlider',
+) as HostComponent<NativeProps>;
--- package/src/index.ts
+++ package/src/index.ts
@@ -1,3 +1,10 @@
-const RNCSlider = require('./RNCSliderNativeComponent').default;
+import {Platform} from 'react-native';
+
+let RNCSlider = null;
+if (Platform.OS == 'ios') {
+  RNCSlider = require('./RNCSliderNativeComponent').default;
+} else {
+  RNCSlider = require('./android/index').default;
+}
 
 export default RNCSlider;
